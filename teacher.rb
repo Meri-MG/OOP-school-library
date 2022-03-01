f1c2730dd644156f7d03d12e4e49f0c5560c3ae5
Meri <mgogi13@freeuni.edu.ge>
2022-03-01 15:57:50 +0400

    Add person, teacher, student classes

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative './person'
+
+class Teacher < Person
+  def initialize(age:, specialization:, parent_permission: true, name: 'Unknown')
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+end
