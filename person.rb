d382fa6e6417032e9a81ac42f2ad66c18624522e
Meri <mgogi13@freeuni.edu.ge>
2022-03-03 14:48:28 +0400

    Add required changes

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require_relative 'corrector'
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
     @age = age
@@ -24,5 +24,10 @@ class Person
 
   def validate_name
     @name = @corrector.correct_name(@name)
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self unless rental.person == self
   end
 end
--- rental.rb
+++ rental.rb
@@ -2,8 +2,7 @@ require_relative './book'
 require_relative './person'
 
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
